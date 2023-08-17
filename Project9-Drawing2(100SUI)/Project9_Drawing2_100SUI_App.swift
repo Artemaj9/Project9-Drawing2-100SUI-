958af30ee0326c328f46314eb739b4bb80d43aae
Artem <moiseev@physics.msu.ru>
2023-08-17 12:20:53 +0300

    Initial Commit

---
+++ Project9-Drawing2(100SUI)/Project9_Drawing2_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Project9_Drawing2_100SUI_App.swift
+//  Project9-Drawing2(100SUI)
+//
+//  Created by Artem on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project9_Drawing2_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
