6eaa6cf0689a4c4a74fe36184f2c84b151189bd4
Artem <moiseev@physics.msu.ru>
2023-08-19 10:25:10 +0300

    animatableData

---
+++ Project9-Drawing2(100SUI)/animatableData.swift
@@ -0,0 +1,49 @@
+//
+//  animatableData.swift
+//  Project9-Drawing2(100SUI)
+//
+//  Created by Artem on 19.08.2023.
+//
+
+import SwiftUI
+
+
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    var animatableData: Double {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
+struct animatableData: View {
+    @State private var insetAmount = 50.0
+    
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    insetAmount = Double.random(in: 10...90)
+                }
+            }
+    }
+}
+
+struct animatableData_Previews: PreviewProvider {
+    static var previews: some View {
+        animatableData()
+    }
+}
