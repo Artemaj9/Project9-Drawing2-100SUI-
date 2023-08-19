99f68b0fb1f7e57495a89701f240d425da8d1259
Artem <moiseev@physics.msu.ru>
2023-08-19 11:22:42 +0300

    spirographView

---
+++ SPirograph.swift
@@ -0,0 +1,21 @@
+//
+//  SPirograph.swift
+//  Project9-Drawing2(100SUI)
+//
+//  Created by Artem on 19.08.2023.
+//
+
+import SwiftUI
+
+
+struct SPirograph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SPirograph_Previews: PreviewProvider {
+    static var previews: some View {
+        SPirograph()
+    }
+}
